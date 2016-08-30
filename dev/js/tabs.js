7726d728de47d76fb663b00c512553a37e5bd6b3
DenisGayda <gayda31den@gmail.com>
2016-08-30 14:37:20 +0300

    First commit tabs

--- dev/js/tabs.js
+++ dev/js/tabs.js
@@ -1,12 +1,19 @@
 function tabs (menu, show) {
 	for (var i = 0; i < menu.length; i++) {
 		menu[i].onclick = function() {
+			style(menu)
 			for (var j = 0; j < show.length; j++) {
 				show[j].style.display = 'none';
 			};
 			document.getElementById(this.dataset.tabs).style.display = 'block';
+			this.classList.add('tabs__menu_active');
 		};
 	};
+	function style (arr) {
+		for (var i = 0; i< arr.length; i++) {
+			arr[i].classList.remove('tabs__menu_active')
+		}
+	}
 };
 
 var menu = document.querySelectorAll('.tabs__menu_item');
